c7f9d650c88dbfce15a8f7bda5678e144c0df615
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-28 12:55:14 -0500

    feat: flow:  - add flow of ViewController -> OtherViewController

---
+++ IOS-Beginning-Swift/OtherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OtherViewController.swift
+//  IOS-Beginning-Swift
+//
+//  Created by OSP on 28/09/17.
+//  Copyright © 2017 josecuentas. All rights reserved.
+//
+
+import UIKit
+
+class OtherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
